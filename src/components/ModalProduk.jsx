bbb29a4f1c4124d7ffcfbbeead1f2f3ca863f05e
Yuda Aradea <yudaaradeagit@gmail.com>
2024-01-25 13:01:08 +0700

    Add Quantity

--- src/components/ModalProduk.jsx
+++ src/components/ModalProduk.jsx
@@ -1,15 +1,28 @@
+import { useState } from "react";
 import { daftarProduct } from "../constants";
+import Swal from "sweetalert2";
 
 const ModalProduk = () => {
+  const [quantity, setQuantity] = useState(1);
+
+  if (quantity < 1) {
+    Swal.fire({
+      title: "Quantity minimal 1",
+      icon: "warning",
+      confirmButtonText: "OK!",
+    });
+    setQuantity(1);
+  }
+
   return (
     <>
       {daftarProduct.map((produk) => (
         <div key={produk.id} className="fp__cart_popup">
-          <div className="modal fade" id={produk.key} tabIndex="-1" aria-hidden="true">
+          <div className="modal fade" id={produk.key} tabIndex="-1" aria-hidden="true" data-bs-backdrop="static">
             <div className="modal-dialog modal-dialog-centered">
               <div className="modal-content">
                 <div className="modal-body">
-                  <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                  <button type="button" onClick={() => setQuantity(1)} className="btn-close" data-bs-dismiss="modal" aria-label="Close">
                     <i className="fa fa-times"></i>
                   </button>
                   <div className="fp__cart_popup_img">
@@ -22,7 +35,7 @@ const ModalProduk = () => {
                     <h4 className="price">{produk.harga}</h4>
 
                     <div className="details_size">
-                      <h5>Spesifikasi Produk</h5>
+                      <h5>Deskripsi Produk</h5>
                       <p>Merk : {produk.merk}</p>
                       <p>Kondisi Penyimpanan : {produk.kondisi}</p>
                       <p>Jenis Makanan : {produk.jenis}</p>
@@ -31,13 +44,52 @@ const ModalProduk = () => {
                       <p>Berat Bersih : {produk.berat}</p>
                     </div>
 
-                    <ul className="details_button_area d-flex flex-wrap justify-content-center">
-                      <li>
-                        <a className="common_btn" href={produk.link} target="_blank" rel="noreferrer">
-                          beli produk
-                        </a>
-                      </li>
-                    </ul>
+                    <div className="details_quentity">
+                      <h5>select quantity</h5>
+                      <div className="quentity_btn_area d-flex flex-wrapa align-items-center">
+                        <div className="quentity_btn">
+                          <button onClick={() => setQuantity(quantity - 1)} className="btn btn-danger">
+                            <i className="fal fa-minus"></i>
+                          </button>
+                          <input type="text" value={quantity} />
+                          <button
+                            onClick={() => {
+                              setQuantity(quantity + 1);
+                              quantity + 1 > produk.stok ? setQuantity(produk.stok) : "";
+                            }}
+                            className="btn btn-success"
+                          >
+                            <i className="fal fa-plus"></i>
+                          </button>
+                        </div>
+                        <h3>{produk.hargaFix * quantity}</h3>
+                      </div>
+                    </div>
+
+                    {produk.stok >= 1 ? (
+                      <ul className="details_button_area d-flex flex-wrap justify-content-center">
+                        <li>
+                          <a
+                            className="common_btn"
+                            href={`https://wa.me/6289671087959/?text=Halo%20suka%20frozen%2C%20saya%20ingin%20memesan%20produk%20%0ANama%20Saya%20%3A...%20%0AAlamat%20saya%20%3A...%20%20%0AProduk%20%3A%20${
+                              produk.nama
+                            }%0AQuantity%20%3A%20${quantity}%0ATotal%20harga%20%3A%20${produk.hargaFix * quantity}`}
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            beli produk
+                          </a>
+                        </li>
+                      </ul>
+                    ) : (
+                      <ul className="details_button_area d-flex flex-wrap justify-content-center">
+                        <li>
+                          <button disabled className="common_btn bg-secondary">
+                            Produk Habis
+                          </button>
+                        </li>
+                      </ul>
+                    )}
                   </div>
                 </div>
               </div>
